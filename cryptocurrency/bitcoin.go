b08144ec4484f296b716d340b7eb4677350834c3
dino <irvin.em@live.com>
2020-03-03 03:09:19 +0800

    fix bug

--- cryptocurrency/bitcoin.go
+++ cryptocurrency/bitcoin.go
@@ -1,5 +1,9 @@
 package cryptocurrency
 
+//PAY ATTENTION
+//implement by btc.com
+//not bitcoin wallet RPC
+
 import (
 	"encoding/json"
 	"errors"
@@ -9,8 +13,10 @@ import (
 	"github.com/btcsuite/btcutil"
 	"github.com/shopspring/decimal"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 type Bitcoin struct {
@@ -59,12 +65,65 @@ type BtcBlockInfo struct {
 	Extras           map[string]interface{} `json:"extras"`
 }
 
-const (
-	BitcoinKeyTypeP2PK  = "P2PK"
-	BitcoinKeyTypeP2PKH = "P2PKH"
+type BtcTxInput struct {
+	PrevAddress  []string `json:"prev_addresses"` //"prev_addresses": Array<String> 输入地址
+	PrevPosition int64    `json:"prev_position"`  // "prev_position": int 前向交易的输出位置
+	PrevTxHash   string   `json:"prev_tx_hash"`   // "prev_tx_hash": string 前向交易哈希
+	PrevValue    int64    `json:"prev_value"`     // "prev_value": int 前向交易输入金额
+	ScriptASM    string   `json:"script_asm"`     // "script_asm": string Script Asm
+	ScriptHex    string   `json:"script_hex"`     // "script_hex": string Script Hex
+	Sequence     int64    `json:"sequence"`       // "sequence": int Sequence
+}
 
+type BtcTxOutput struct {
+	Addresses []string `json:"addresses"`  // addresses: Array<String> 输出地址
+	Value     int64    `json:"value"`      // value: int 输出金额
+	PayType   string   `json:"type"`       // "type": "P2PKH"
+	ScriptASM string   `json:"script_asm"` // "script_asm": string Script Asm
+	ScriptHex string   `json:"script_hex"` // "script_hex": string Script Hex
+	//"script_asm": "OP_DUP OP_HASH160 89950b23ec273834ca18a75fc28580d48b0232a8 OP_EQUALVERIFY OP_CHECKSIG",
+	//"script_hex": "76a91489950b23ec273834ca18a75fc28580d48b0232a888ac",
+	SpentByTx         string `json:"spent_by_tx"`          // "spent_by_tx": null
+	SpentByTxPosition int64  `json:"spent_by_tx_position"` // "spent_by_tx_position": -1
+}
+
+type BtcTransaction struct {
+	BlockHeight  int64          `json:"block_height"` // block_height: int 所在块高度
+	BlockTime    int64          `json:"block_time"`   // block_time: int 所在块时间
+	CreatedAt    int64          `json:"created_at"`   // created_at: int 该记录系统处理时间，没有业务含义
+	Fee          int64          `json:"fee"`          // fee: int 该交易的手续费
+	Hash         string         `json:"hash"`         // hash: string 交易哈希
+	Inputs       []*BtcTxInput  `json:"inputs"`
+	InputsCount  int64          `json:"inputs_count"` // inputs_count: int 输入数量
+	InputsValue  int64          `json:"inputs_value"` // inputs_value: int 输入金额
+	IsCoinbase   bool           `json:"is_coinbase"`  // is_coinbase: boolean 是否为 coinbase 交易
+	LockTime     int64          `json:"lock_time"`    // lock_time: int lock time
+	Outputs      []*BtcTxOutput `json:"outputs"`
+	OutputsCount int64          `josn:"outputs_count"` // outputs_count: int 输出数量
+	OutputsValue int64          `json:"outputs_value"` // outputs_value: int 输出金额
+	Size         int64          `json:"size"`          // size: int 交易体积
+	Version      int64          `json:"version"`       // version: int 交易版本号
+	//others data from response
+	BlockHash     string `json:"block_hash"`
+	Confirmations int64  `json:"confirmations"`
+	IsDoubleSpent bool   `json:"is_double_spend"`
+	IsSWTx        bool   `json:"is_sw_tx"`
+	Weight        int64  `json:"weight"`
+	Vsize         int64  `json:"vsize"`
+	WitnessHash   string `json:"witness_hash"`
+	SigOps        int64  `json:"sigops"`
+}
+
+type BtcTxPage struct {
+	TotalCount int64             `json:"total_count"`
+	Page       int64             `json:"page"`
+	PageSize   int64             `json:"pagesize"`
+	Txs        []*BtcTransaction `json:"list"`
+}
+
+const (
+	BitcoinKeyTypeP2PKH = "P2PKH"
 	BitcoinKeyTypeP2SH  = "P2SH"
-	BitcoinKeyTypeP2SHH = "P2SHH"
 
 	BitcoinKeyTypeP2WPKH = "P2WPKH"
 	BitcoinKeyTypeP2WSH  = "P2WSH"
@@ -109,42 +168,51 @@ func (b *Bitcoin) AllocAccount(password, salt string, addrType interface{}) (add
 	privKey := wif.String()
 
 	switch pubKeyType {
-	//case BitcoinKeyTypeP2PK:
-	//	var p2pk *btcutil.AddressPubKey
-	//	if p2pk,err = btcutil.NewAddressPubKey(btcutil.Hash160(wif.SerializePubKey()),bitNet);err!=nil{
-	//		return
-	//	}
-	//	addr = p2pk.EncodeAddress()
 	case BitcoinKeyTypeP2PKH: // 1***
 		var p2pkh *btcutil.AddressPubKeyHash
 		if p2pkh, err = btcutil.NewAddressPubKeyHash(btcutil.Hash160(wif.SerializePubKey()), b.net); err != nil {
 			return
 		}
+		//log.Printf("addr=\t%s\n", p2pkh.EncodeAddress())
+		//log.Printf("priv=\t%s\n", privKey)
+		//log.Printf("puk=\t%s\n", hex.EncodeToString(wif.SerializePubKey()))
+		//log.Printf("pubHash=\t%s\n\n", hex.EncodeToString(p2pkh.ScriptAddress()))
+
 		addr = p2pkh.EncodeAddress()
 	case BitcoinKeyTypeP2SH: // 3***
 		var p2sh *btcutil.AddressScriptHash
 		if p2sh, err = btcutil.NewAddressScriptHash(btcutil.Hash160(wif.SerializePubKey()), b.net); err != nil {
 			return
 		}
+		//log.Printf("addr=\t%s\n", p2sh.EncodeAddress())
+		//log.Printf("priv=\t%s\n", privKey)
+		//log.Printf("puk=\t%s\n", hex.EncodeToString(wif.SerializePubKey()))
+		//log.Printf("pubHash=\t%s\n\n", hex.EncodeToString(p2sh.ScriptAddress()))
+
 		addr = p2sh.EncodeAddress()
-	//case BitcoinKeyTypeP2SHH:	// 3***
-	//	var p2shh *btcutil.AddressScriptHash
-	//	if p2shh, err = btcutil.NewAddressScriptHashFromHash(btcutil.Hash160(wif.SerializePubKey()), bitNet); err != nil {
-	//		return
-	//	}
-	//	addr = p2shh.EncodeAddress()
 	case BitcoinKeyTypeP2WPKH: // bc1***
 		var p2wpkh *btcutil.AddressWitnessPubKeyHash
 		if p2wpkh, err = btcutil.NewAddressWitnessPubKeyHash(btcutil.Hash160(wif.SerializePubKey()), b.net); err != nil {
 			return
 		}
+		//log.Printf("addr=\t%s\n", p2wpkh.EncodeAddress())
+		//log.Printf("priv=\t%s\n", privKey)
+		//log.Printf("puk=\t%s\n", hex.EncodeToString(wif.SerializePubKey()))
+		//log.Printf("pubHash=\t%s\n\n", hex.EncodeToString(p2wpkh.ScriptAddress()))
+
 		addr = p2wpkh.EncodeAddress()
-	//case BitcoinKeyTypeP2WSH:
-	//	var p2wsh *btcutil.AddressWitnessScriptHash
-	//	if p2wsh,err = btcutil.NewAddressWitnessScriptHash(btcutil.Hash160(wif.SerializePubKey()),bitNet);err!=nil{
-	//		return
-	//	}
-	//	addr = p2wsh.EncodeAddress()
+	case BitcoinKeyTypeP2WSH:
+		var p2wsh *btcutil.AddressWitnessScriptHash
+		buf := wif.SerializePubKey()
+		if p2wsh, err = btcutil.NewAddressWitnessScriptHash(buf[1:], b.net); err != nil {
+			return
+		}
+		//log.Printf("priv=\t%s\n", privKey)
+		//log.Printf("puk=\t%s\n", hex.EncodeToString(wif.SerializePubKey()))
+		//log.Printf("addr=\t%s\n", p2wsh.EncodeAddress())
+		//log.Printf("pubHash=\t%s\n\n", hex.EncodeToString(p2wsh.ScriptAddress()))
+
+		addr = p2wsh.EncodeAddress()
 	default:
 		err = fmt.Errorf("key type %s is unsupport", pubKeyType)
 		return
@@ -205,12 +273,81 @@ func (b *Bitcoin) BlockByHash(blkHash string) (bi interface{}, err error) {
 		return
 	}
 	bi = bbi
-	return}
+	return
+}
 
 //transaction
-func (b *Bitcoin) TransactionsInBlocks(from, to uint64) (txs []*TransactionRecord, err error) { return }
-func (b *Bitcoin) MakeTransaction([]*TxFrom, []*TxTo) (txSigned interface{}, err error)       { return }
-func (b *Bitcoin) SendTransaction(txSigned interface{}) (txHash string, err error)            { return }
+func (b *Bitcoin) TransactionsInBlocks(from, to uint64) (txs []*TransactionRecord, err error) {
+	//https://chain.api.btc.com/v3/block/latest/tx?verbose=2
+	//verbose，可选，默认为2，选择输出内容等级，含义分别如下：
+	//等级 1，包含交易信息；
+	//等级 2，包含等级 1、交易的输入、输出地址与金额；
+	//等级 3，包含等级 2、交易的输入、输入 script 等信息。
+	txs, err = b.getBlkTxs(from)
+	return
+}
+
+func (b *Bitcoin) getBlkTxs(blk uint64) (txs []*TransactionRecord, err error) {
+	var (
+		btp    *BtcTxPage
+		page   int64 = 1
+		amount decimal.Decimal
+	)
+	for { //each page
+		btp = &BtcTxPage{}
+		if err = b.request(fmt.Sprintf("/block/%d/tx?verbose=3&page=%d", blk, page), btp); err != nil {
+			return
+		}
+		//don't consider of page size,because:i don't know how to setup
+		log.Printf("%d --> %d\n", blk, page)
+		for _, v := range btp.Txs { //each transaction
+			for index, o := range v.Outputs { //each output
+				if o.PayType == "NULL_DATA" {
+					log.Printf("txid=%s to=%v, value=%d type=%s\n", v.Hash, o.Addresses, o.Value, o.PayType)
+					continue //skip NULL_DATA
+				}
+				if o.PayType == "P2PKH_MULTISIG" {
+					log.Printf("txid=%s to=%v, value=%d type=%s\n", v.Hash, o.Addresses, o.Value, o.PayType)
+					continue // skip P2PKH_MULTISIG
+				}
+				if len(o.Addresses) > 1 {
+					log.Printf("txid=%s to=%v, value=%d type=%s\n", v.Hash, o.Addresses, o.Value, o.PayType)
+					continue //skip multi sign
+				}
+				log.Printf("txid=%s to=%v, value=%d type=%s\n", v.Hash, o.Addresses, o.Value, o.PayType)
+				if amount, err = ToBtc(o.Value); err != nil {
+					log.Printf("transfer amount failed, txid=%s, o_index=%d\n", v.Hash, index)
+					continue
+				}
+				tx := &TransactionRecord{
+					TokenFlag:   b.Symbol(),
+					Index:       uint64(index),
+					Value:       amount,
+					To:          o.Addresses[0],
+					BlockHash:   v.BlockHash,
+					TxHash:      v.Hash,
+					BlockNumber: uint64(v.BlockHeight),
+					TimeStamp:   v.BlockTime,
+				}
+				if strings.HasPrefix(o.PayType, BitcoinKeyTypeP2WPKH) {
+					log.Println("todo: format address")
+				}
+				txs = append(txs, tx)
+			}
+		}
+		//break
+		if btp.PageSize > int64(len(btp.Txs)) {
+			break
+		} else {
+			page++
+		}
+	}
+	//log.Printf("%+v\n", btp)
+	return
+}
+
+func (b *Bitcoin) MakeTransaction([]*TxFrom, []*TxTo) (txSigned interface{}, err error) { return }
+func (b *Bitcoin) SendTransaction(txSigned interface{}) (txHash string, err error)      { return }
 func (b *Bitcoin) MakeAgentTransaction(from string, agent []*TxFrom, to []*TxTo) (txSigned interface{}, err error) {
 	return
 }
--- cryptocurrency/btc_test.go
+++ cryptocurrency/btc_test.go
@@ -1,7 +1,9 @@
 package cryptocurrency
 
 import (
+	"encoding/hex"
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil"
 	"github.com/shopspring/decimal"
 	"testing"
 )
@@ -17,24 +19,18 @@ func TestCryptoCurrencyBitcoin(t *testing.T) {
 	}
 	t.Log("name=", cc.CoinName())
 
-	a, p, err := cc.AllocAccount("passwordpassword", "salt", BitcoinKeyTypeP2PK)
-	t.Logf("P2PK\taddr=%s\tpriv=%s\terr=%v\n", a, p, err)
-
-	a, p, err = cc.AllocAccount("passwordpassword", "salt", BitcoinKeyTypeP2PKH)
+	a, p, err := cc.AllocAccount("passwordpassword", "salt", BitcoinKeyTypeP2PKH)
 	t.Logf("P2PKH\taddr=%s\tpriv=%s\terr=%v\n", a, p, err)
 
 	a, p, err = cc.AllocAccount("passwordpassword", "salt", BitcoinKeyTypeP2SH)
 	t.Logf("P2SH\taddr=%s\tpriv=%s\terr=%v\n", a, p, err)
-
-	a, p, err = cc.AllocAccount("passwordpassword", "salt", BitcoinKeyTypeP2SHH)
-	t.Logf("P2SHH\taddr=%s\tpriv=%s\terr=%v\n", a, p, err)
 
 	a, p, err = cc.AllocAccount("passwordpassword", "salt", BitcoinKeyTypeP2WPKH)
 	t.Logf("P2WPKH\taddr=%s\tpriv=%s\terr=%v\n", a, p, err)
 
 	a, p, err = cc.AllocAccount("passwordpassword", "salt", BitcoinKeyTypeP2WSH)
 	t.Logf("P2WSH\taddr=%s\tpriv=%s\terr=%v\n", a, p, err)
-
+	return
 	//check addr valid
 	addrs := []string{
 		"12sQsrgs3Ypo6MqbaYHRKs7ADh8oMphWhC",
@@ -74,10 +70,66 @@ func TestCryptoCurrencyBitcoin(t *testing.T) {
 	t.Logf("last block= %d %v\n", blkNum, err)
 
 	//get block by number
-	bi, err := cc.BlockByNumber(619476)
+	bi, err := cc.BlockByNumber(blkNum - 1)
 	t.Logf("blk content: %+v %v\n", bi, err)
 
 	//get block by hash
-	bi,err = cc.BlockByHash("00000000000000000010ff7ad8443865c89f2de3047e0c5d7f84dedd44e666b5")
+	bi, err = cc.BlockByHash("00000000000000000010ff7ad8443865c89f2de3047e0c5d7f84dedd44e666b5")
 	t.Logf("blk content: %+v %v\n", bi, err)
+
+	//get tx in blocks
+	txs, err := cc.TransactionsInBlocks(blkNum-3, blkNum)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, v := range txs {
+		t.Logf("txid=%s to=%s index=%d amount=%s\n", v.TxHash, v.To, v.Index, v.Value.String())
+	}
+}
+
+func TestDecodeAddress(t *testing.T) {
+	ad, er := btcutil.DecodeAddress("bc1qwqdg6squsna38e46795at95yu9atm8azzmyvckulcc7kytlcckxswvvzej", &chaincfg.MainNetParams)
+	t.Log(ad, er)
+	t.Log(hex.EncodeToString(ad.ScriptAddress()))
+	ad, er = btcutil.DecodeAddress("bc1qj382exhwmuys2ps7jhpjdxfdjeylws0qxw8tql", &chaincfg.MainNetParams)
+	t.Log(ad, er)
+	t.Log(hex.EncodeToString(ad.ScriptAddress()))
+	ad, er = btcutil.DecodeAddress("3CaqMax7uJpjo28w8t9n15FwhzRNQaaE1f", &chaincfg.MainNetParams)
+	t.Log(ad, er)
+	t.Log(hex.EncodeToString(ad.ScriptAddress()))
+	ad, er = btcutil.DecodeAddress("164ibT2TVy6CD7xuzXvcqCPMrMm3jGVtxS", &chaincfg.MainNetParams)
+	t.Log(ad, er)
+	t.Log(hex.EncodeToString(ad.ScriptAddress()))
+
+	return
+}
+
+func TestEncodePubkey(t *testing.T) {
+	//e2b765cf8ff1d1738b3d63951c98e04d2af1da7f		1MfmR3rMYnUBzGfpjnUdNW6RGFrMBgv7Jx
+	serializedPubKey, er := hex.DecodeString("e2b765cf8ff1d1738b3d63951c98e04d2af1da7f")
+	if er != nil {
+		t.Fatal(er)
+	}
+	t.Log("1--->", len(serializedPubKey))
+	buf, er := btcutil.NewAddressPubKeyHash(serializedPubKey, &chaincfg.MainNetParams)
+	t.Log("2--->", buf, er)
+
+	//944eac9aeedf0905061e95c326992d9649f741e0		bc1qj382exhwmuys2ps7jhpjdxfdjeylws0qxw8tql
+	serializedPubKey, er = hex.DecodeString("944eac9aeedf0905061e95c326992d9649f741e0")
+	if er != nil {
+		t.Fatal(er)
+	}
+	t.Log("1--->", len(serializedPubKey))
+	buf1, er := btcutil.NewAddressWitnessPubKeyHash(serializedPubKey, &chaincfg.MainNetParams)
+	t.Log("2--->", buf1, er)
+
+	//701a8d401c84fb13e6baf169d59684e17abd9fa216c8cc5b9fc63d622ff8c58d		bc1qwqdg6squsna38e46795at95yu9atm8azzmyvckulcc7kytlcckxswvvzej
+	serializedPubKey, er = hex.DecodeString("701a8d401c84fb13e6baf169d59684e17abd9fa216c8cc5b9fc63d622ff8c58d")
+	if er != nil {
+		t.Fatal(er)
+	}
+	t.Log("1--->", len(serializedPubKey))
+	buf2, er := btcutil.NewAddressWitnessScriptHash(serializedPubKey, &chaincfg.MainNetParams)
+	t.Log("2--->", buf2, er)
+	return
 }
